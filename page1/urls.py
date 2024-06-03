9c8bbcde1ff6e8783252fde408f71e0c734c676d
Debabrata Samanta <samanta.drdsr@gmail.com>
2024-06-04 05:15:54 +0530

    update all html page

--- page1/urls.py
+++ page1/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # for headers url ....
     path('',views.home,name='home'),
-    path('innerpage/',views.innerpage,name='innerpage'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('more/',views.more,name='more'),
+    path('service/',views.service,name='service'),
+
+    # for questions url ....
+    path('innerpage/',views.innerpage),
 
 ]
--- page1/views.py
+++ page1/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html',{})
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def more(request):
+    return render(request,'more.html')
+
+def service(request):
+    return render(request,'service.html')
 
 def innerpage(request):
-    return render(request,'innerpage.html',{})
+    return render(request,'innerpage.html')
