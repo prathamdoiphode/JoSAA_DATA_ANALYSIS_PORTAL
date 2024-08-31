a5e6b77f73c1925bec85e74640b61cc1422f642b
prathamdoiphode <prathamrdoiphode@gmail.com>
2024-09-01 00:31:56 +0530

    host

--- josaa/settings.py
+++ josaa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2!2br66hiu4c*)pbv5^w5-$h!)=2z7b%do04m^&&k(#!@gfiqb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
